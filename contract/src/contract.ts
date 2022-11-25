52d53914d1d693282d9726ea829cf1b747138f41
Roy Sun <roy.sun@sunrecruitment.co.nz>
2022-11-25 21:40:54 +1300

    first commit

---
+++ contract/src/contract.ts
@@ -0,0 +1,21 @@
+import { NearBindgen, near, call, view, LookupMap, initialize } from 'near-sdk-js';
+
+@NearBindgen({})
+class NFTMinting {
+  owner_id: string;
+  owner_by_id: LookupMap;
+  token_id: number;
+
+  constructor() {
+    this.owner_id = "";
+    this.owner_by_id = new LookupMap("n");
+    this.token_id = 0;
+  }
+
+  @initialize({})
+  init({owner_id, prefix}){
+    this.owner_id = owner_id;
+    this.owner_by_id = new LookupMap(prefix);
+    this.token_id = 0;
+  }
+}
